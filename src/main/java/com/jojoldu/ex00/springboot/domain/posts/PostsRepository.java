c566b244c55b5d215acd308a0374e25968addd1b
Pjh <jiho5993@naver.com>
2020-06-23 21:53:06 +0900

    Add mustache CRUD

--- src/main/java/com/jojoldu/ex00/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/ex00/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jojoldu.ex00.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> { // JpaRepository<Entity, PK>
     // Entity 클래스와 기본 Entity Repository는 함께 위치해야함
+
+    @Query("select p from Posts p order by p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/ex00/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/ex00/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.ex00.springboot.service.posts;
 
 import com.jojoldu.ex00.springboot.domain.posts.Posts;
 import com.jojoldu.ex00.springboot.domain.posts.PostsRepository;
+import com.jojoldu.ex00.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.ex00.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.ex00.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.ex00.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -40,6 +44,23 @@ public class PostsService {
         );
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회기능만 남겨두어 조회 속도가 개선됨.
+    public List<PostsListResponseDto> findAllDesc() {
+        // postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환하는 메소드
+       return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+
+        postsRepository.delete(posts);
     }
 
 }
